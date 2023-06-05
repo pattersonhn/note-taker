6d48590469c723d0c37df7abf1ced55648b705d0
pattersonhn <pattersonhn01@gmail.com>
2023-06-05 13:57:31 -0400

    fixed post

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { notes } = require('../../db/db');
+let  notes = require('../../db/db');
 const { createNewNote, findById, editNote, removeNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
