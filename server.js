2659c1a0cf77f659c7c847e734cf88c86b8e503f
pattersonhn <pattersonhn01@gmail.com>
2023-06-05 14:02:44 -0400

    update

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
